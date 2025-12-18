d915f90c6f4856b13f97f3451fc3f160707fc46a
chiffon1204 <github@chiffon-lab.coffee>
2025-12-19 00:45:32 +0900

    feat: add footer component and initial posts index file

--- advent-calendar-web/components/layout/Footer.tsx
+++ advent-calendar-web/components/layout/Footer.tsx
@@ -1,27 +1,18 @@
 export function Footer() {
     return (
         <footer className="border-t border-orange-100 bg-orange-50/30 py-12 md:py-16">
-            <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-                <p className="text-center text-sm leading-loose text-gray-500 md:text-left">
-                    Built by{' '}
+            <div className="container mx-auto flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
+                <p className="text-center text-sm leading-loose text-gray-500">
+                    Copyright© 2025{' '}
                     <a
-                        href="#"
+                        href="https://buicha.social/@Chiffon1204"
                         target="_blank"
                         rel="noreferrer"
                         className="font-medium text-orange-600 underline underline-offset-4 hover:text-orange-500"
                     >
-                        You
+                        ChiffonLaboratory
                     </a>
-                    . The source code is available on{' '}
-                    <a
-                        href="#"
-                        target="_blank"
-                        rel="noreferrer"
-                        className="font-medium text-orange-600 underline underline-offset-4 hover:text-orange-500"
-                    >
-                        GitHub
-                    </a>
-                    .
+                    {' '}All Rights Reserved.
                 </p>
             </div>
         </footer>
